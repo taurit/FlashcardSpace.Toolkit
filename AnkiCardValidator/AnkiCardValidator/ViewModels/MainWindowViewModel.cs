8062027cf552028ae6ab66be663314d6ceac330c
Paweł Bulwan <github@taurit.pl>
2024-07-07 22:20:05 +0200

    Update penalty function

--- AnkiCardValidator/AnkiCardValidator/ViewModels/MainWindowViewModel.cs
+++ AnkiCardValidator/AnkiCardValidator/ViewModels/MainWindowViewModel.cs
@@ -77,7 +77,7 @@ public sealed class FlashcardViewModel(
         (this.CefrLevel >= CefrClassification.C1 ? 1 : 0) +
 
         // the more individual meanings word has, the more confusing learning it with flashcards might be
-        Meanings.Count +
+        (Meanings.Count > 0 ? Meanings.Count - 1 : 0) +
 
         // ChatGPT raised at least one quality issue
         (HasQualityIssues ? 1 : 0) +
@@ -86,7 +86,27 @@ public sealed class FlashcardViewModel(
         DuplicatesOfFrontSide.Count +
 
         // word appears to have duplicates in the deck (back side)
-        DuplicatesOfBackSide.Count
+        DuplicatesOfBackSide.Count +
 
+        // no frequency data - this can be false negative, if term is a sentence, or HTML tags weren't sanitized.
+        // I can improve false alarms with heuristics
+        (FrequencyPositionFrontSide.HasValue ? 0 : 1) +
+        (FrequencyPositionBackSide.HasValue ? 0 : 1) +
+
+        // frequency data exists and suggests that Spanish word is used very infrequently
+        (FrequencyPositionFrontSide.HasValue ? CalculateFrequencyPenalty(FrequencyPositionFrontSide.Value) : 0) +
+
+        // same for polish side
+        (FrequencyPositionBackSide.HasValue ? CalculateFrequencyPenalty(FrequencyPositionBackSide.Value) : 0)
         ;
+
+    private int CalculateFrequencyPenalty(int position) => position switch
+    {
+        < 5000 => 0,
+        < 10000 => 1,
+        < 15000 => 2,
+        < 20000 => 3,
+        < 25000 => 4,
+        _ => 5
+    };
 }
