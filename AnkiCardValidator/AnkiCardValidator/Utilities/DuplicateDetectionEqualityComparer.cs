0f0695ba763fed77e1218191687e5b9e000c7523
Paweł Bulwan <github@taurit.pl>
2024-07-07 14:37:56 +0200

    Add cache to duplicate detection for significant performance boost

---
+++ AnkiCardValidator/AnkiCardValidator/Utilities/DuplicateDetectionEqualityComparer.cs
@@ -0,0 +1,54 @@
+﻿namespace AnkiCardValidator.Utilities;
+
+public class DuplicateDetectionEqualityComparer : IEqualityComparer<string>
+{
+    private readonly Dictionary<string, string> _normalizedStringsCache = new();
+
+    public bool Equals(string? x, string? y)
+    {
+        if (x is null || y is null)
+        {
+            return false;
+        }
+
+        var xNormalized = ConvertToComparableForm(x);
+        var yNormalized = ConvertToComparableForm(y);
+        return xNormalized == yNormalized;
+    }
+
+    public int GetHashCode(string obj)
+    {
+        return ConvertToComparableForm(obj).GetHashCode();
+    }
+
+    private string ConvertToComparableForm(string input)
+    {
+        if (!_normalizedStringsCache.ContainsKey(input))
+        {
+            // remove punctuation
+            var sanitized = new string(input.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            // remove leading/trailing whitespaces
+            sanitized = sanitized.Trim();
+
+            // lowercase
+            var lowercase = sanitized.ToLowerInvariant();
+
+            // remove preceding articles
+            var wordsToRemove = new[] { "el", "la", "los", "las", "un", "una", "unos", "unas" };
+            foreach (var wordToRemove in wordsToRemove)
+            {
+                if (lowercase.StartsWith(wordToRemove + " "))
+                {
+                    lowercase = lowercase.Substring(wordToRemove.Length + 1);
+                }
+            }
+
+            // trim what's left
+            var trimmed = lowercase.Trim();
+            _normalizedStringsCache[input] = trimmed;
+        }
+
+        return _normalizedStringsCache[input];
+    }
+}
