41c556a904c5f7b95360f69b0b8eb69dd66227ad
Paweł Bulwan <github@taurit.pl>
2024-08-23 23:40:56 +0200

    Implement SRT text content parse/denoise

---
+++ src/CoreLibrary.Tests/Utilities/SubtitlesHelper.Srt.Tests.cs
@@ -0,0 +1,108 @@
+﻿using CoreLibrary.Utilities;
+using FluentAssertions;
+
+namespace CoreLibrary.Tests.Utilities;
+
+[TestClass]
+public class SubtitlesHelperTests
+{
+    [TestMethod]
+    public void When_SubtitlesArePreprocessed_Expect_SequenceNumberIsIgnored()
+    {
+        // Arrange
+        var subtitles = @"1
+            00:00:00,000 --> 00:00:02,000
+            Hello
+            
+            2
+            00:00:02,000 --> 00:00:04,000
+            World";
+
+        // Act
+        var text = SubtitlesHelperSrt.ConvertSubtitlesToText(subtitles);
+
+        // Assert
+        text.Should().NotContain("1");
+    }
+
+    [TestMethod]
+    public void When_SubtitlesArePreprocessed_Expect_TimestampLinesAreNotPreserved()
+    {
+        // Arrange
+        var subtitles = @"1
+            00:00:00,000 --> 00:00:02,000
+            Hello
+            
+            2
+            00:00:02,000 --> 00:00:04,000
+            World";
+
+        // Act
+        var text = SubtitlesHelperSrt.ConvertSubtitlesToText(subtitles);
+
+        // Assert
+        text.Should().NotContain("00:00");
+        text.Should().NotContain("-->");
+    }
+
+    [TestMethod]
+    public void When_SubtitlesArePreprocessed_Expect_TextIsPreserved()
+    {
+        // Arrange
+        var subtitles = @"1
+            00:00:00,000 --> 00:00:02,000
+            Hello
+            
+            2
+            00:00:02,000 --> 00:00:04,000
+            World";
+
+        // Act
+        var text = SubtitlesHelperSrt.ConvertSubtitlesToText(subtitles);
+
+        // Assert
+        text.Should().Contain("Hello");
+        text.Should().Contain("World");
+    }
+
+    [TestMethod]
+    public void When_SubtitlesArePreprocessed_Expect_MultipleWhitespaceCharactersAreReducedToSingleSpace()
+    {
+        // Arrange
+        var subtitles = @"1
+            00:00:00,000 --> 00:00:02,000
+            Hello
+            
+            2
+            00:00:02,000 --> 00:00:04,000
+            World";
+
+        // Act
+        var text = SubtitlesHelperSrt.ConvertSubtitlesToText(subtitles);
+
+        // Assert
+        text.Should().NotContain("  ");
+        text.Should().NotContain("\t ");
+        text.Should().NotContain("\t\t");
+        text.Should().NotContain(" \r");
+        text.Should().NotContain(" \n");
+        text.Should().NotContain("\r ");
+        text.Should().NotContain("\n ");
+    }
+
+    [TestMethod]
+    public void When_RealWorldSubtitlesArePreprocessed_Expect_NoExceptions()
+    {
+        // Arrange
+        var subtitles = File.ReadAllText("Resources/NightOfTheLivingDead.srt");
+
+        // Act
+        var text = SubtitlesHelperSrt.ConvertSubtitlesToText(subtitles);
+
+        // Assert
+        text.Should().NotStartWith("-->", because: "Timestamps should be all removed");
+        text.Should().NotContain("\r\n\r\n", because: "There should be no empty lines in the text");
+        text.Should().NotContain("\n\n", because: "There should be no empty lines in the text");
+    }
+
+}
---
+++ src/CoreLibrary/Utilities/SubtitlesHelper.Srt.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CoreLibrary.Utilities;
+
+public static partial class SubtitlesHelperSrt
+{
+    [GeneratedRegex(@"^\s*\d+\s*$|^\s*\d{2}:\d{2}:\d{2},\d{3}.*$", RegexOptions.Multiline | RegexOptions.Compiled)]
+    private static partial Regex UnwantedSubstrings();
+
+    /// <summary>
+    /// Removes all timestamps and sequence numbers from the subtitles file content.
+    /// Leaves only subtitle text (also without html markup).
+    /// </summary>
+    /// <remarks>
+    /// *.srt format contains blocks like:
+    /// <code>
+    /// 1
+    /// 00:00:00,835 --&gt; 00:00:03,254
+    /// Viserys Targaryen:
+    /// I, Viserys Targaryen,
+    ///
+    /// 2
+    /// 00:00:03,796 --&gt; 00:00:05,757
+    /// Lord of the Seven Kingdoms,
+    /// </code>
+    ///
+    /// There is a room for inconsistency, and Video Players generally rely on the timestamps, not the sequence number or order of blocks.
+    /// In theory, I should then order the blocks by timestamps, but let's try to not mess with the order and just assume that
+    /// the blocks are already ordered by timestamps (why wouldn't they be?).
+    /// 
+    /// </remarks>
+    public static string ConvertSubtitlesToText(string subtitlesFileContent)
+    {
+        var cleanedContent = UnwantedSubstrings().Replace(subtitlesFileContent, string.Empty);
+
+        // reduce whitespace character strings to single space
+        cleanedContent = Regex.Replace(cleanedContent, @"\s{2,}", " ");
+
+        return cleanedContent.Trim();
+    }
+}
