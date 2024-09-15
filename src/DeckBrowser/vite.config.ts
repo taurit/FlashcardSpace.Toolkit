6cc0c398f5444eac748752fb3c2c71b3f4f19640
Paweł Bulwan <github@taurit.pl>
2024-09-15 16:22:55 +0200

    Commit the build version of Deck Browser

--- src/DeckBrowser/vite.config.ts
+++ src/DeckBrowser/vite.config.ts
@@ -1,11 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  //base: "/demo/", // use this if you want to serve from a subfolder. Also, add it in tasks.json or relative URLs won't work as expected!
-  base: "/",
-  esbuild: { legalComments: "none" },
+    plugins: [
+        react(),
+        // viteSingleFile() // this is mostly for aesthetics; not sure whether to turn it on or off yet. Git diffs are cleaner when it's off.
+    ],
+    //base: "/demo/", // use this if you want to serve from a subfolder. Also, add it in tasks.json or relative URLs won't work as expected!
+    base: "/",
+    esbuild: { legalComments: "none" },
 });
