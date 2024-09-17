cae6c533d733b75ffb24b63816c8ba9ae6275e14
Paweł Bulwan <github@taurit.pl>
2024-09-17 13:09:25 +0200

    Update Deck Browser to allow display preview of a deck on a smartphone

--- src/DeckBrowser/src/main.tsx
+++ src/DeckBrowser/src/main.tsx
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom/client";
 import DeckPreviewWindow from "./DeckPreviewWindow.tsx";
 
 // added to make debugging in Firefox responsibe/mobile rendering mode easier
-const codeToInjectWhenRanAsStandaloneApp = import.meta.env.DEV && <meta name="viewport" content="width=device-width, initial-scale=1" />;
+const codeToInjectWhenRanAsStandaloneApp = (
+    //import.meta.env.DEV &&
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
