47547bc1c7bec5aa5bca2f24d2c4727958b40552
Paweł Bulwan <github@taurit.pl>
2024-09-25 14:39:54 +0200

    Add model for quality control of the final flashcard

---
+++ src/GenerateFlashcards.Tests/Models/Spanish/FlashcardQualityImprovementSuggestionTests.cs
@@ -0,0 +1,99 @@
+﻿using CoreLibrary.Services.ObjectGenerativeFill;
+using FluentAssertions;
+using GenerateFlashcards.Models.Spanish;
+using GenerateFlashcards.Tests.Infrastructure;
+using GenerateFlashcards.Tests.TestInfrastructure;
+
+namespace GenerateFlashcards.Tests.Models.Spanish;
+
+[TestClass]
+[TestCategory("RequiresGenerativeAi")]
+public class FlashcardQualityImprovementSuggestionTests
+{
+    private readonly GenerativeFill _generativeFill = GenerativeFillTestFactory.CreateInstance();
+
+    [TestMethod]
+    public async Task When_FlashcardIsSimpleAndCorrect_Expect_NoWarning()
+    {
+        // Arrange
+        var flashcard = new FlashcardQualityImprovementSuggestion
+        {
+            FrontTextSpanish = "Hola",
+            BackTextPolish = "Cześć",
+            SentenceExampleSpanish = "Hola, ¿cómo estás?",
+            SentenceExamplePolish = "Cześć, jak się masz?"
+        };
+
+        // Act
+        var output = await _generativeFill.FillMissingProperties(TestParameters.OpenAiModelId, TestParameters.OpenAiModelId, flashcard);
+
+        // Assert
+        output.Dump();
+        output.Suggestions.Should().Be("");
+    }
+
+    [TestMethod]
+    public async Task When_FlashcardContainsErrorInDefiniteArticle_Expect_WarningIsPresent()
+    {
+
+        // Arrange
+        var flashcard = new FlashcardQualityImprovementSuggestion
+        {
+            FrontTextSpanish = "el casa", // error in definite article! should be la casa.
+            BackTextPolish = "dom",
+            SentenceExampleSpanish = "La casa es bonita.",
+            SentenceExamplePolish = "Dom jest ładny."
+        };
+
+        // Act
+        var output = await _generativeFill.FillMissingProperties(TestParameters.OpenAiModelId, TestParameters.OpenAiModelId, flashcard);
+
+        // Assert
+        output.Dump();
+        output.Suggestions.Should().NotBeEmpty();
+        output.Suggestions.Should().Contain("casa");
+    }
+
+    [TestMethod]
+    public async Task When_FlashcardContainsSubtleErrorLikeMissingNationalCharacters_Expect_WarningIsPresent()
+    {
+
+        // Arrange
+        var flashcard = new FlashcardQualityImprovementSuggestion
+        {
+            FrontTextSpanish = "la casa",
+            BackTextPolish = "dom",
+            SentenceExampleSpanish = "La casa es bonita.",
+            SentenceExamplePolish = "Dom jest ladny."
+        };
+
+        // Act
+        var output = await _generativeFill.FillMissingProperties(TestParameters.OpenAiModelId, TestParameters.OpenAiModelId, flashcard);
+
+        // Assert
+        output.Dump();
+        output.Suggestions.Should().NotBeEmpty();
+        output.Suggestions.Should().Contain("ladny");
+    }
+
+    [TestMethod]
+    public async Task When_TranslationIsIncorrect_Expect_WarningIsPresent()
+    {
+        // Arrange
+        var flashcard = new FlashcardQualityImprovementSuggestion
+        {
+            FrontTextSpanish = "la casa",
+            BackTextPolish = "lalka",
+            SentenceExampleSpanish = "La casa es bonita.",
+            SentenceExamplePolish = "Lalka jest ładna."
+        };
+
+        // Act
+        var output = await _generativeFill.FillMissingProperties(TestParameters.OpenAiModelId, TestParameters.OpenAiModelId, flashcard);
+
+        // Assert
+        output.Dump();
+        output.Suggestions.Should().NotBeEmpty();
+        output.Suggestions.Should().Contain("dom");
+    }
+}
---
+++ src/GenerateFlashcards/Models/Spanish/FlashcardQualityImprovementSuggestion.cs
@@ -0,0 +1,25 @@
+﻿using CoreLibrary.Services.ObjectGenerativeFill;
+using System.ComponentModel.DataAnnotations;
+
+namespace GenerateFlashcards.Models.Spanish;
+
+class FlashcardQualityImprovementSuggestion : ObjectWithId
+{
+    [Key]
+    public string FrontTextSpanish { get; init; }
+
+    public string BackTextPolish { get; init; }
+
+    public string SentenceExampleSpanish { get; init; }
+
+    public string SentenceExamplePolish { get; init; }
+
+    [FillWithAI]
+    [FillWithAIRule($"This field contains suggestions for flashcard deck author on how to improve the flashcard quality.")]
+    [FillWithAIRule("It helps with quality assurance oif the flashcard deck.")]
+    [FillWithAIRule($"Flashcard is to be used by a native Polish student learning Spanish vocabulary.")]
+    [FillWithAIRule($"If any flashcard property contains typos, grammar errors, non-existing words or other quality issues, provide brief suggestion to reject the flashcard, or how to fix the issue.")]
+    [FillWithAIRule($"If the flashcard is correct and has acceptable quality, leave this field empty.")]
+    public string Suggestions { get; init; }
+
+}
